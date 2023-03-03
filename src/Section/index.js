7a03401d6c429cca2d8b45d3d86c56517f8e9371
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2023-03-03 13:32:15 +0100

    task list in react without interaction yet

---
+++ src/Section/index.js
@@ -0,0 +1,18 @@
+import "./style.css";
+
+const Section = ({title ,body ,extraHeaderContent}) => (
+
+    <section className="section">
+        <header className="section__header" ></header>
+            <h2 className="section__title">{title}</h2>
+            {extraHeaderContent}
+
+        <div className="section__body">
+            {body}
+        </div>
+    </section>
+
+);
+
+
+export default Section;
